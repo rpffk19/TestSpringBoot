9783554c16c4cab6768dbfe6518a1f98792bb8fd
rpffk19 <rpffk1@gmail.com>
2024-03-12 09:48:50 +0900

    TEST COMMIT

---
+++ TestSpringBoot/src/test/java/com/yun/edu/TestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yun.edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
